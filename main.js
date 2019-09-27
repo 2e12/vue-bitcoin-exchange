0c6ccd848b62b4d3e9fbb1338b5339fcd44b55f7
2e12 <gabriel.beutler@4teamwork.ch>
2019-09-27 15:09:29 +0200

    Added reload button.

--- main.js
+++ main.js
@@ -3,19 +3,36 @@ var app = new Vue({
   data: {
     info: null,
     error: false,
+    data_url: "https://api.coindesk.com/v1/bpi/currentprice.json"
   },
-  mounted () {
-    let that = this
-    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(data) {
-      that.info = data
-    })
-    .catch(function(error) {
-      that.error = true
-      console.log(error);
-    });
-  }
+  methods:{
+    loadData: function(url, callback){
+      fetch(url)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        callback(data)
+      })
+      .catch(function(error) {
+        that.error = true
+        console.log(error)
+      });
+    },
+    refresh: function () {
+      this.error = false
+      this.info = null
+      this.loadData(this.data_url, (data) => {
+        this.info = data
+      })
+    }
+  },
+  computed:{
+    isLoading: function () {
+      return !this.info
+    }
+  },
+  mounted: function() {
+    this.refresh()
+  },
 })
